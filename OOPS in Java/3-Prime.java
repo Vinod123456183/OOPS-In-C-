0868bb8ecc1bbfd89f5e62280864de073a9ce31a
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-03-24 08:12:07 +0530

    Create 3-Prime.java

---
+++ OOPS in Java/3-Prime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+
+    static class Prime {
+        int n = 0 ;
+        
+        void getdata()  {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Value of N ");
+            n = sc.nextInt();
+        }
+
+        void putdata() {
+            int i = 1 , count = 0;
+            for(; i<= n; i++)  {
+                if(n%i == 0)  {
+                    count++;
+                }
+            }
+
+            if(count  ==  2)  {
+                System.out.println("Yes");
+            } else  {
+                System.out.println("! Yes");
+            }
+        }
+
+    }
+
+    public static void main(String [] args)  {
+        Prime aa = new Prime();
+        aa.getdata();
+        aa.putdata();
+    }
+}
