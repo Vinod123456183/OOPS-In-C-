a6ac6fa1e1761c937f6d3165b8ffd5ed04aa8409
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-03-24 12:42:42 +0530

    Create 4-static.java

---
+++ OOPS in Java/4-static.java
@@ -0,0 +1,29 @@
+
+class Main
+{
+    static class A 
+    {
+        int a ;
+        static String s = "GEhu";
+        
+        A(int aa ) 
+        {
+            a = aa;
+        }
+        
+        void display() 
+        {
+            System.out.println(" " + a);
+            System.out.println(" "  + s);
+        }
+    
+    }
+    public static void main(String [] args) 
+    {
+        A aaa = new A(10) ;
+        aaa.display();
+        
+        A bbb = new A(20);
+        bbb.display();
+    }
+}
