6f4f236b329527435b44ea2877158717e031df4e
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-03-26 23:52:19 +0530

    Create 9-method-overriding.java

---
+++ OOPS in Java/9-method-overriding.java
@@ -0,0 +1,24 @@
+// DErived class ne base class k function ko overwrite krna  
+// To avoid it use final keyword on class A final void Print(){ // }
+
+class A {
+  void Print()  {
+        System.out.println("Inside base Class");
+  }
+}
+
+class B extends A {
+    
+  void Print()  {
+        System.out.println("Inside Derived Class");
+  }
+}
+
+class Main {
+    public static void main (String [] args)  {
+
+    B aa = new B();
+    aa.Print();
+    
+}
+}
