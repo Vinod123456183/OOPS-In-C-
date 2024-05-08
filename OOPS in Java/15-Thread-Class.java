03f6cabeb206c7c7a5e0315d15ed719df8e4eb93
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-05-08 22:19:59 +0530

    Update 15-Thread-Class.java

--- OOPS in Java/15-Thread-Class.java
+++ OOPS in Java/15-Thread-Class.java
@@ -2,24 +2,26 @@
 // E O C S
 
 
-/*
- Creating  Thread Using Thread CLass
- */
-
+/* Creating  Thread Using Thread CLass*/
+//  we can say This Program is Single Task Single Thread
+// For Single Task Multiple thread Create an object  
 
 //  Step 1 -> Extend Thread Class
 public class Main extends Thread {
     // Step 2  OverRide Run Method
-    public void run() {
-        System.out.println("Thread Is Running......");
+    public void run() {     //task 1
+        System.out.println("Thread Is Running......"); 
     }
 
     public static void main(String[] args) {
         // Step 3 Create Object Of Class 
-        Main t = new Main();
-
+        Main t = new Main();    //Thread 1
         // Step 4 - Start Thread
         t.start();
 
+        Main t2 = new Main();
+        t2.start();   // Thread 2  , 
+
+     // Both Thread Will Call run So it Will called Single Task Multiple Thread
     }
 }
