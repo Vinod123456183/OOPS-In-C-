1c5909f6dabffbe25a75eb66f437337c120b61bf
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-05-08 18:11:24 +0530

    Create 12-Without-Threading.java

---
+++ OOPS in Java/12-Without-Threading.java
@@ -0,0 +1,12 @@
+// Note Before runnig Program file name Should Tc.java
+
+class Tc{
+    public static void main(String[]args) {
+        int a = 100 ; 
+        int b = 0 ; 
+        int c = a/b;
+        System.err.println(c);
+        System.err.println("\nThis Should Be Printed\n");
+        System.err.println("\nProgram Is terminated By JVM\n");
+    }
+}
