5d3204eaf4a042e0ce3240dd47ad9fcfd1e75755
Vinod <vinodsinghbarti420@gmail.com>
2025-08-19 17:04:02 +0530

    Input And Output Function Using getline and funx

---
+++ Q-3-Input-Output.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Car  {
+
+    public:
+
+    string car_Name;
+    string car_Color;
+    int car_Price;
+
+    void input()  {
+        cout << "Enter Car Name : ";
+        getline(cin , car_Name);
+        cout << "Enter Car Color : ";
+        getline(cin , car_Color);
+        cout << "Enter Car Price : ";
+        cin >> car_Price;
+    
+        cin.ignore();
+    }
+    
+
+    void display() {
+        cout << "Car Name :" << car_Name << endl;
+        cout << "Car Color :" << car_Color << endl;
+        cout << "clsCar Price :" << car_Price << endl;
+    }
+
+};
+
+int main ()  {
+
+    Car C1;
+    C1.input();
+    C1.display();
+
+}
