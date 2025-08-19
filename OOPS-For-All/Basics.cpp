ade7d0015d034502cf2505418fa2b03e45e1e3e9
Vinod <vinodsinghbarti420@gmail.com>
2025-08-19 15:47:06 +0530

    Basics Of Class

--- OOPS-For-All/Basics.cpp
+++ OOPS-For-All/Basics.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Car{
+	public:
+        string brand;
+        string model;
+        int year;
+};
+
+int main () {
+
+
+	Car C1 , C2; 
+
+	C1.year = 2866;
+	C1.brand = "BMW";
+	C1.model = "NEW Model";
+
+
+	cout << C1.brand << " " << C1.model << " " <<C1.year;
+	cout<<endl;	
+    
+}
