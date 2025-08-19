ff3ad75180f29cdba2e4edb8e335e5c8fef325ae
Vinod <vinodsinghbarti420@gmail.com>
2025-08-19 16:09:15 +0530

    Calculate Size of Class Using Padding

---
+++ OOPS-For-All/Q-2-Size-of-class-padding.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Car{
+	public:
+	double sd;
+	int cc;
+	char aa;
+	char bc;
+	char bcd;
+	char bcsd;
+	char bcdsa;
+	char bdewc;
+	char bcdsdfs;
+	char bcsgfd;
+	char bcdssdfa;
+};
+
+int main () {
+
+	Car C1 , C2; 
+	cout << sizeof(Car);
+	cout<<endl;
+	cout << sizeof(C1);
+
+
+}
