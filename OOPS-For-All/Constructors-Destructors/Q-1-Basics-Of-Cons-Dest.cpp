f68f69aefe899fc262dd2804cfc254b64dba731a
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-08-23 23:21:16 +0530

    Update Q-1-Basics-Of-Cons-Dest.cpp

--- OOPS-For-All/Constructors-Destructors/Q-1-Basics-Of-Cons-Dest.cpp
+++ OOPS-For-All/Constructors-Destructors/Q-1-Basics-Of-Cons-Dest.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class customers{
+    public:
+    string name;
+    int rno;
+    
+    customers(string name_,int rno_ ) {
+        name=name_;
+        rno=rno_;
+    }
+    
+  void ans() {
+        cout << name << " " << rno;
+    }  
+};
+int main ()  {
+    // we cannot manulay paas bec it is a function
+    customers c("V",10);
+    c.ans();
+    
+}
