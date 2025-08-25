4bc94acd2bbd1d582c27798094b6259b8fa00d7c
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-08-25 11:15:00 +0530

    Create Q-10-Ambiguity-Resolution-In-Inheritance.cpp

---
+++ OOPS-For-All/Constructors-Destructors/Q-10-Ambiguity-Resolution-In-Inheritance.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class A {
+  public:
+  
+    void display() {
+        cout << "inside A Display " << endl;
+    }
+};
+
+
+class B : public A {
+    public:
+    void display(){
+        cout << "Inside B display " << endl;
+        // A::display();
+    }
+};
+
+int main()  {
+    B b;
+    b.display();
+    b.A::display();
+}
+
+
+
